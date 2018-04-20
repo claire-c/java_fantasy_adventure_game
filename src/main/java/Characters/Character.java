268cc45f7cd7e5eb9146761d5ea43b217f199f9b
Claire <cconnachan@gmail.com>
2018-04-20 20:22:35 +0100

    wrote setup for Character abstract class

---
+++ src/main/java/Characters/Character.java
@@ -0,0 +1,29 @@
+package Characters;
+
+public abstract class Character {
+
+    private String name;
+    private int hp;
+
+    public Character(String name){
+        this.name = name;
+        this.hp = 100;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+
+    
+
+
+}
