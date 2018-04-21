f2f7a2053e17f3aac5e834ffcbe5c16b85b5b865
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-21 22:41:17 +0100

    Barbarian signature move hard coded to be half as successful as his weapons chance value.

--- src/main/java/Characters/Heroes/Fighters/Barbarian.java
+++ src/main/java/Characters/Heroes/Fighters/Barbarian.java
@@ -1,4 +1,19 @@
 package Characters.Heroes.Fighters;
 
-public class Barbarian {
+import Characters.Character;
+
+public class Barbarian extends Fighter {
+
+    public Barbarian(String name){
+        super(name);
+        weapon = Weapon.CLUB;
+        defence = Defence.ARMGUARDS;
+    }
+
+    public void signatureMove(Character characterToAttack){
+        if (shouldDoMove(weapon.chanceValue / 2))
+        {characterToAttack.setHp(0);
+        this.setHp(0);
+        }
+    }
 }
