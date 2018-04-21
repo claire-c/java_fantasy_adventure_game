78350d49356f027a5cbfa199562c5a4f59d65f9a
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-22 00:39:46 +0100

    Started work on room class and room test. Lost time not realising that import Characters.Character doesn't happen automatically. Might be an issue with the oackages layout?

--- src/main/java/Room/Room.java
+++ src/main/java/Room/Room.java
@@ -1,4 +1,42 @@
 package Room;
 
+import Characters.Character;
+import Characters.Creatures.Creature;
+import Characters.Creatures.CreatureType;
+import Characters.Heroes.Treasure;
+
+import java.util.ArrayList;
+
 public class Room {
+
+    private Character monster;
+    private Treasure treasure;
+    private ArrayList<Character> heroes;
+
+
+    public Room(){
+        this.monster = new Creature(CreatureType.getRandomCreature());
+        this.treasure = Treasure.getRandomTreasure();
+        this.heroes = new ArrayList<>();
+    }
+
+    public Character getMonster() {
+        return monster;
+    }
+
+    public Treasure getTreasure() {
+        return treasure;
+    }
+
+    public ArrayList<Character> getHeroes() {
+        return heroes;
+    }
+
+    public Boolean noHeroesAlive(){
+        return heroes.isEmpty();
+    }
+
+//    public ArrayList<Character> encounter( ArrayList<Character> heroes){
+//
+//    }
 }
--- src/test/java/RoomTest.java
+++ src/test/java/RoomTest.java
@@ -1,2 +1,67 @@
+import Characters.Creatures.Creature;
+import Characters.Creatures.CreatureType;
+import Characters.Heroes.Fighters.Knight;
+import Characters.Heroes.Healers.Cleric;
+import Characters.Heroes.Mages.Spell;
+import Characters.Heroes.Mages.Warlock;
+import Characters.Character;
+import org.junit.Before;
+import Room.Room;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class RoomTest {
+    private Room room1;
+    private Character hero1;
+    private Character hero2;
+    private Character hero3;
+    private Creature creature1;
+    private ArrayList<Character> heroes;
+
+    @Before
+    public void before(){
+        creature1 = new Creature(CreatureType.GIANTSPIDER);
+        room1 = new Room();
+        hero1 = new Knight("Peter");
+        hero2 = new Warlock("Ashley", Spell.FIREBALL, creature1);
+        hero3 = new Cleric("Pawel");
+        ArrayList<Character> heroes = new ArrayList<>();
+        heroes.add(hero1);
+        heroes.add(hero2);
+        heroes.add(hero3);
+    }
+
+    @Test
+    public void canAssert(){
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void hasCreature(){
+        assertNotNull(room1.getMonster());
+    }
+
+    @Test
+    public void hasTreasure(){
+        assertNotNull(room1.getTreasure());
+    }
+
+    @Test
+    public void heroesStartEmpty(){
+        assertEquals(0, room1.getHeroes().size());
+    }
+
+    @Test
+    public void startNoHeroesAlive(){
+        assertEquals(true, room1.noHeroesAlive());
+    }
+
+//    @Test
+//    public void canFight(){
+//        room1.encounter(heroes);
+//    }
 }
