11bbf2ba72b19ef83b9fd793283886a6f5420ce2
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-22 20:37:52 +0100

    Game Class constructor updated to add 5 rooms to rooms ArrayList. Need to test they have been successfully populated by random creatues.

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -9,11 +9,22 @@ public class Game {
     private String name;
     private ArrayList<Room> rooms;
     private ArrayList<Hero> heroes;
+    private Room room1;
+    private Room room2;
+    private Room room3;
+    private Room room4;
+    private Room room5;
 
     public Game(String name){
         this.name = name;
-        this.rooms = new ArrayList<>();
         this.heroes = new ArrayList<>();
+        ArrayList<Room> generatedRooms = new ArrayList<>();
+        generatedRooms.add(room1);
+        generatedRooms.add(room2);
+        generatedRooms.add(room3);
+        generatedRooms.add(room4);
+        generatedRooms.add(room5);
+        this.rooms = generatedRooms;
     }
 
     public String getName() {
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import Game.Game;
+import Characters.Character;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameTest {
+    private Game game1;
+
+    @Before
+    public void before(){
+        game1 =  new Game("M&C's Castle of Madness");
+    }
+
+    @Test
+    public void canAssert(){
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("M&C's Castle of Madness", game1.getName());
+    }
+
+    @Test
+    public void hasRandomRooms(){
+        assertEquals(5, game1.getRooms().size());
+    }
+
+    @Test
+    public void roomsHaveCreatures(){
+
+    }
 }
